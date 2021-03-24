8f5ee19b541243b73a204132a587bb1824539933
gabrielgoisandrade <gabriel.gois.andrade14@gmail.com>
2021-03-24 04:21:02 -0300

    Subindo projeto refatorado

--- src/main/kotlin/com/gga/webhook/repositories/RepositoryRepository.kt
+++ src/main/kotlin/com/gga/webhook/repositories/RepositoryRepository.kt
@@ -1,9 +1,7 @@
 package com.gga.webhook.repositories
 
 import com.gga.webhook.models.RepositoryModel
-import com.gga.webhook.models.UserModel
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface RepositoryRepository : JpaRepository<RepositoryModel, Long> {
